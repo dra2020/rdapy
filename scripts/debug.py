fbbcf90737cd29e2651ec9c6ce1eaa1774a4510a
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-05-09 07:12:06 -0700

    Added contiguity unit tests

--- scripts/debug.py
+++ scripts/debug.py
@@ -13,8 +13,8 @@ plan_path = "testdata/graph/SAMPLE-BG-map.csv"
 graph_path = "testdata/graph/SAMPLE-BG-graph.json"
 
 
-def check_embeddedness(plan_path: str, graph_path: str) -> bool:
-    """Test helper for is_embedded()"""
+def check_contiguity(plan_path: str, graph_path: str) -> bool:
+    """Test helper for is_connected()"""
 
     plan_rows = read_csv(plan_path, [str, int])
     graph = read_json(graph_path)
@@ -24,16 +24,16 @@ def check_embeddedness(plan_path: str, graph_path: str) -> bool:
     for k, v in plan.items():
         inverted_plan[v].add(k)
 
-    for id, features in inverted_plan.items():
-        embedded: bool = is_embedded(id, plan, inverted_plan, graph)
+    for id, geos in inverted_plan.items():
+        connected: bool = is_connected(list(geos), graph)
 
-        if embedded:
+        if not connected:
             return False
 
     return True
 
 
-assert check_embeddedness(plan_path, graph_path)
+assert check_contiguity(plan_path, graph_path)
 
 pass
 
--- test/test_graph.py
+++ test/test_graph.py
@@ -9,6 +9,26 @@ from rdapy.graph import *
 from testutils import *
 
 
+def check_contiguity(plan_path: str, graph_path: str) -> bool:
+    """Test helper for is_connected()"""
+
+    plan_rows = read_csv(plan_path, [str, int])
+    graph = read_json(graph_path)
+
+    plan: dict[str, int | str] = {row["GEOID"]: row["DISTRICT"] for row in plan_rows}
+    inverted_plan: defaultdict[int | str, set[str]] = defaultdict(set)
+    for k, v in plan.items():
+        inverted_plan[v].add(k)
+
+    for id, geos in inverted_plan.items():
+        connected: bool = is_connected(list(geos), graph)
+
+        if not connected:
+            return False
+
+    return True
+
+
 def check_embeddedness(plan_path: str, graph_path: str) -> bool:
     """Test helper for is_embedded()"""
 
@@ -57,6 +77,36 @@ class TestGraph:
             "c": ["a", "b"],
         }
         assert is_connected(geos, adjacency)
+
+    def test_is_connected_BG(self) -> None:
+        plan_path = "testdata/graph/SAMPLE-BG-map.csv"
+        graph_path = "testdata/graph/SAMPLE-BG-graph.json"
+        assert check_contiguity(plan_path, graph_path)
+
+        plan_path = "testdata/graph/SAMPLE-BG-map-discontiguous.csv"
+        graph_path = "testdata/graph/SAMPLE-BG-graph.json"
+        assert not check_contiguity(plan_path, graph_path)
+
+    def test_is_connected_GRID(self) -> None:
+        plan_path = "testdata/graph/grid-4-square.csv"
+        graph_path = "testdata/graph/grid-graph.json"
+        assert check_contiguity(plan_path, graph_path)
+
+        plan_path = "testdata/graph/grid-stray-1.csv"
+        graph_path = "testdata/graph/grid-graph.json"
+        assert not check_contiguity(plan_path, graph_path)
+
+        plan_path = "testdata/graph/grid-stray-2.csv"
+        graph_path = "testdata/graph/grid-graph.json"
+        assert not check_contiguity(plan_path, graph_path)
+
+        plan_path = "testdata/graph/grid-corner.csv"
+        graph_path = "testdata/graph/grid-graph.json"
+        assert check_contiguity(plan_path, graph_path)
+
+        plan_path = "testdata/graph/grid-donut.csv"
+        graph_path = "testdata/graph/grid-graph.json"
+        assert check_contiguity(plan_path, graph_path)
 
     def test_is_embedded_BG(self) -> None:
         plan_path = "testdata/graph/SAMPLE-BG-map.csv"
