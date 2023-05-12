8c0b92c0699fc6d60f7cf4442e820e60913edfea
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-05-12 06:42:52 -0700

    WIP

--- rdapy/partisan/method.py
+++ rdapy/partisan/method.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 """
+The core elements of John Nagle's method.
+
+Formulas:
+
+* Estimate seat probability
+* Estimate district responsiveness
+
 Metrics:
 
-* S! = the estimated number of Democratic seats using first past the post
 * S# = the estimated Democratic seats, using seat probabilities
-* S% = the estimated Democratic seat share fraction, calculated as S# / N
+* S! = the estimated number of Democratic seats using first past the post
 
-TODO
+Seats-votes curves:
+
+* Infer SV points
+* Infer inverse SV points
+* Infer geometric seats bias points
 """
 
 from math import erf, sqrt, isclose
@@ -36,13 +46,13 @@ def est_district_responsiveness(vpi: float) -> float:
 
 
 def est_seats(Vf_array: list[float]) -> float:
-    """The estimated # of Democratic seats, using seat probabilities"""
+    """S# - The estimated # of Democratic seats, using seat probabilities"""
 
     return sum([est_seat_probability(vpi) for vpi in Vf_array])
 
 
-def est_statewide_seats_fptp(Vf_array: list[float]) -> float:
-    """The estimated # of Democratic seats, using first past the post"""
+def est_seats_fptp(Vf_array: list[float]) -> float:
+    """S! - The estimated # of Democratic seats, using first past the post"""
 
     return sum([1.0 for vpi in Vf_array if (vpi > 0.5)])
 
@@ -50,78 +60,75 @@ def est_statewide_seats_fptp(Vf_array: list[float]) -> float:
 # INFER AN S/V CURVE
 
 
-def infer_sv_points(
-    statewide_vote_share, vpi_by_district, proportional=True, fptp=False
-):
+def infer_sv_points(Vf: float, Vf_array: list[float], proportional=True) -> list[tuple]:
+    """Infer the points of an S/V curve from a set of VPI's by district,
+    using either proportional or uniform shifts in statewide vote share."""
+
     sv_curve_pts = []
-    vpis_at_half_share = []
 
-    for shifted_vote_share in shift_range(statewide_vote_share):
-        shifted_vpis = shift_districts(
-            statewide_vote_share, vpi_by_district, shifted_vote_share, proportional
-        )
-        shifted_seats = est_statewide_seats(shifted_vpis, fptp)
-        sv_curve_pts.append((shifted_vote_share, shifted_seats))
+    for shifted_Vf in shift_range(Vf):
+        shifted_Vf_array = _shift_districts(Vf, Vf_array, shifted_Vf, proportional)
+        shifted_Sf = est_seats(shifted_Vf_array)
+        sv_curve_pts.append((shifted_Vf, shifted_Sf))
 
-        # Squirrel away the inferred VPIs by district at V = 0.5
-        if isclose(shifted_vote_share, 0.5):
-            vpis_at_half_share = shifted_vpis
-
-    return sv_curve_pts, vpis_at_half_share
+    return sv_curve_pts
 
 
-# SHIFT DISTRICTS EITHER PROPORTIONALLY OR UNIFORMLY
+def _shift_districts(
+    Vf: float, Vf_array: list[float], shifted_Vf: float, proportional: bool = True
+) -> list[float]:
+    """Shift the VPI's by district, using either proportional or uniform shifts in statewide vote share."""
 
-
-def shift_districts(
-    statewide_vote_share, vpi_by_district, shifted_vote_share, proportional=True
-):
     if proportional:
-        return shift_districts_proportionally(
-            statewide_vote_share, vpi_by_district, shifted_vote_share
-        )
+        return _shift_districts_proportionally(Vf, Vf_array, shifted_Vf)
     else:
-        return shift_districts_uniformly(
-            statewide_vote_share, vpi_by_district, shifted_vote_share
-        )
+        return _shift_districts_uniformly(Vf, Vf_array, shifted_Vf)
 
 
-def shift_districts_uniformly(
-    statewide_vote_share, vpi_by_district, shifted_vote_share
-):
-    shift = shifted_vote_share - statewide_vote_share
-    shifted_vpis = [(v + shift) for v in vpi_by_district]
+def _shift_districts_uniformly(
+    Vf: float, Vf_array: list[float], shifted_Vf: float
+) -> list[float]:
+    """Shift the VPI's by district, using uniform shifts in statewide vote share."""
+
+    shift: float = shifted_Vf - Vf
+    shifted_vpis: list[float] = [(v + shift) for v in Vf_array]
 
     return shifted_vpis
 
 
-def shift_districts_proportionally(
-    statewide_vote_share, vpi_by_district, shifted_vote_share
-):
-    if shifted_vote_share < statewide_vote_share:
+def _shift_districts_proportionally(
+    Vf: float, Vf_array: list[float], shifted_Vf: float
+) -> list[float]:
+    """Shift the VPI's by district, using proportional shifts in statewide vote share."""
+
+    proportion: float
+    shifted_Vf_array: list[float]
+
+    if shifted_Vf < Vf:
         # Shift down: D's to R's
-        proportion = shifted_vote_share / statewide_vote_share
-        shifted_vpis = [(v * proportion) for v in vpi_by_district]
-    elif shifted_vote_share > statewide_vote_share:
+        proportion = shifted_Vf / Vf
+        shifted_Vf_array = [(v * proportion) for v in Vf_array]
+    elif shifted_Vf > Vf:
         # Shift up: R's to D's
-        proportion = (1 - shifted_vote_share) / (1 - statewide_vote_share)
-        shifted_vpis = [(1 - (1 - v) * proportion) for v in vpi_by_district]
+        proportion = (1 - shifted_Vf) / (1 - Vf)
+        shifted_Vf_array = [(1 - (1 - v) * proportion) for v in Vf_array]
     else:
         # No shift: shift = actual
-        shifted_vpis = vpi_by_district
+        shifted_Vf_array = Vf_array
 
-    return shifted_vpis
-
-
-# Infer inverse S/V curve
+    return shifted_Vf_array
 
 
-def infer_inverse_sv_points(ndistricts, statewide_vote_share, sv_pts):
-    inverse_sv_curve_pts = []
+def infer_inverse_sv_points(
+    N: int, Vf: float, sv_pts: list[tuple]
+) -> list[tuple[float, float]]:
+    """Infer the points of an inverse S/V curve from a set of S/V curve points."""
+
+    inverse_sv_curve_pts: list[tuple[float, float]] = []
 
     for v_d, s_d in sv_pts:
-        v_r = 1 - v_d
-        s_r = ndistricts - s_d  # # of seats, not seat share!
+        v_r: float = 1 - v_d
+        s_r: float = N - s_d  # of seats, not seat share!
         inverse_sv_curve_pts.append((v_r, s_r))
 
     inverse_sv_curve_pts = sorted(inverse_sv_curve_pts, key=lambda pt: [pt[0]])
@@ -129,18 +136,23 @@ def infer_inverse_sv_points(ndistricts, statewide_vote_share, sv_pts):
     return inverse_sv_curve_pts
 
 
-# Infer a Bias of Geometric Seats (B_GS) curve
+def infer_geometric_seats_bias_points(
+    d_sv_pts: list[tuple[float, float]], r_sv_pts: list[tuple[float, float]]
+) -> list[tuple[float, float]]:
+    """Infer a Bias of Geometric Seats (B_GS) curve"""
 
+    b_gs_pts: list[tuple[float, float]] = []
 
-def infer_geometric_seats_bias_points(n_pts, d_sv_pts, r_sv_pts):
-    b_gs_pts = []
+    for i in range(0, len(d_sv_pts)):
+        v_r: float
+        s_r: float
+        v_d: float
+        s_d: float
 
-    for i in range(0, n_pts):
         v_r, s_r = r_sv_pts[i]
         v_d, s_d = d_sv_pts[i]
 
-        # NOTE - By convention: '+' = R bias; '-' = D bias
-        b_gs = 0.5 * (s_r - s_d)
+        b_gs: float = 0.5 * (s_r - s_d)
 
         b_gs_pts.append((v_d, b_gs))
 
