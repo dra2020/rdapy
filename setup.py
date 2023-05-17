78f2f9de5144c3e996d7711374650ede2c38c565
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-05-17 09:17:09 -0700

    First pip install-able package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 """
 python setup.py register
@@ -7,13 +7,20 @@ python setup.py register sdist upload
 
 setup(
     name="rdapy",
-    version="1.0",
+    version="1.0.2",
     description="Redistricting analytics in Python",
     url="https://github.com/dra2020/rdapy",
     author="alecramsay",
     author_email="alec@davesredistricting.org",
     license="MIT",
-    packages=["rdapy"],
+    packages=[
+        "rdapy",
+        "rdapy.compactness",
+        "rdapy.equal",
+        "rdapy.graph",
+        "rdapy.partisan",
+        "rdapy.splitting",
+    ],
     install_requires=[
         "attrs",
         "certifi",
