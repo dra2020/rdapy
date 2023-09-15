74815d31cfff3482e73059054925b4e917e1ce4d
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-09-15 08:02:09 -0700

    Reading census data

--- samples/from_raw_data.py
+++ samples/from_raw_data.py
@@ -12,7 +12,61 @@ from testutils import *
 xx: str = "NC"
 data_path: str = "~/local/sample-data"
 
+do_assignments: bool = False
+do_census: bool = False
+do_elections: bool = True
 do_shapes: bool = False
+
+# Helpers
+
+
+def read_census(rel_path: str) -> defaultdict[str, int]:
+    """Read the DRA census block data JSON for a state."""
+
+    abs_path: str = FileSpec(rel_path).abs_path
+    with open(abs_path, "r", encoding="utf-8-sig") as f:
+        data: Any = json.load(f)
+
+    dataset_key: str = "D20F"
+    field: str = "Tot"
+    pop_by_geoid: defaultdict[str, int] = defaultdict(int)
+    for feature in data["features"]:
+        geoid: str = feature["properties"]["GEOID"]
+        pop: int = feature["properties"]["datasets"][dataset_key][field]
+
+        pop_by_geoid[geoid] = pop
+
+    return pop_by_geoid
+
+
+# Assignments
+
+if do_assignments:
+    plan_path: str = os.path.expanduser(f"{data_path}/NC_2022_Congress_Official.csv")
+    assignments = read_csv(plan_path, [str, int])
+
+    # TODO: More
+
+    pass
+
+# Census data
+
+if do_census:
+    census_path: str = os.path.expanduser(
+        f"{data_path}/2020vt_Census_block_37_data2.json"
+    )
+    population: dict[str, int] = read_census(census_path)
+
+    # TODO: More
+
+    pass
+
+# Elections
+
+if do_elections:
+    # TODO: get this data
+
+    pass
 
 # Shapes
 
@@ -23,4 +77,8 @@ if do_shapes:
 
     # TODO: More
 
+    pass
+
 pass
+
+### END ###
