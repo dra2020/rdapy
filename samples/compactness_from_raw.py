e4cd4e9009a611b7632c9f4a3eea2f6e5a934eaf
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-09-16 15:15:32 -0700

    WIP

--- samples/compactness_from_raw.py
+++ samples/compactness_from_raw.py
@@ -4,6 +4,10 @@
 Sample compactness analytics starting from raw data
 """
 
+import pandas as pd
+import geopandas
+from geopandas import GeoDataFrame
+
 from rdapy import *
 from testutils import *
 
@@ -14,7 +18,8 @@ data_dir: str = "~/local/sample-data"
 # Exported from DRA
 plan_file: str = "NC_2022_Congress_Official.csv"
 
-# This are the Census TIGER/Line block shape for the state.
+district_shapes_path: str = f"testdata/compactness/NC-116th-Congressional"
+# This are the Census TIGER/Line block shapes for the state.
 shapes_file: str = "tl_2020_37_tabblock20"
 
 ## 1 - READ A BLOCK-ASSIGNMENT FILE ###
@@ -22,16 +27,34 @@ shapes_file: str = "tl_2020_37_tabblock20"
 plan_path: str = os.path.expanduser(f"{data_dir}/{plan_file}")
 plan = read_csv(plan_path, [str, int])
 
-### 2 - READ THE BLOCK SHAPES ###
+### 2 - READ THE DISTRICT SHAPES OR CONSTRUCT THEM FROM BLOCK SHAPES ###
 
+# Load the district shapes from a shapefile:
+shapes_path: str = os.path.expanduser(f"{district_shapes_path}")
+shapes, _ = load_shapes(shapes_path, id="id")
+shapes = [item[1] for item in shapes]  # discard the id
+
+# Or construct them from block shapes:
 shapes_path: str = os.path.expanduser(f"{data_dir}/{shapes_file}")
-shapes, _ = load_shapes(shapes_path, id="GEOID20")
+blocks_gdf: GeoDataFrame = geopandas.read_file(shapes_path)
+blocks_df: pd.Series[Any] | pd.DataFrame | Any = blocks_gdf[["geometry", "GEOID20"]]
+assert isinstance(blocks_df, pd.DataFrame)
+
+# TODO - HERE
+regions_gdf: GeoDataFrame = geopandas.read_file(regions_baf_path)
+regions_df: pd.Series[Any] | pd.DataFrame | Any = regions_gdf[["GEOID", "REGION"]]
+assert isinstance(regions_df, pd.DataFrame)
 
 ### 3 - COLLAPSE BLOCKS INTO DISTRICT SHAPES ###
 
 ### 4 - COMPUTE COMPACTNESS METRICS ###
 
+results: dict = calc_compactness(shapes)
+
 ### 5 - PRINT THE RESULTS ###
+
+print(f"Partisan compactness analytics:")
+print(results)
 
 pass
 
