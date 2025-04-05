54c9dbda8ebdf480d753523aa943faa1d9188ac5
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-04-05 06:41:08 -0700

    WIP

--- scripts/SCORE.sh
+++ scripts/SCORE.sh
@@ -2,6 +2,7 @@
 
 # Default values
 GEOJSON=""
+DATA_MAP=""
 GRAPH=""
 PLANS=""
 SCORES=""
@@ -12,6 +13,10 @@ while [[ $# -gt 0 ]]; do
   case "$1" in
     --geojson)
       GEOJSON="$2"
+      shift 2
+      ;;
+    --data-map)
+      DATA_MAP="$2"
       shift 2
       ;;
     --graph)
@@ -38,20 +43,43 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check if all required arguments are provided
-if [[ -z "$GEOJSON" || -z "$GRAPH" || -z "$PLANS" || -z "$SCORES" || -z "$BY_DISTRICT" ]]; then
+if [[ -z "$GEOJSON" || -z "$DATA_MAP" || -z "$GRAPH" || -z "$PLANS" || -z "$SCORES" || -z "$BY_DISTRICT" ]]; then
   echo "Error: Missing required arguments"
-  echo "Usage: SCORE.sh --geojson <path> --graph <path> --plans <path> --scores <path> --by-district <path>"
+  echo "Usage: SCORE.sh --geojson <path> --data-map <path> --graph <path> --plans <path> --scores <path> --by-district <path>"
   exit 1
 fi
 
 # Echo the required messages
 echo
-echo "Extracting data from ${GEOJSON} ..."
+echo "Extracting data from ${GEOJSON} using ${DATA_MAP}..."
+echo
+
+# scripts/extract_data.py \
+# --geojson testdata/data/NC_vtd_datasets.geojson \
+# --data-map testdata/data/NC_data_map.json \
+# --graph testdata/intermediate/NC_graph.json \
+# --data temp/DEBUG_input_data.jsonl
+
+echo
 echo "Extracted graph at ${GRAPH} ..."
+echo
+
+echo
 echo "Plans to be score at ${PLANS} ..."
+echo
+
+echo
 echo "Aggregating data by district ..."
+echo
+
+echo
 echo "Scoring plans ..."
-echo "Writing scores to ${SCORES}..."
-echo "Writing aggregates to ${BY_DISTRICT} ..."
+echo
+
+echo
+echo "Writing scores to ${SCORES} and aggregates to ${BY_DISTRICT} ..."
+echo
+
+echo
 echo "Done!"
 echo
