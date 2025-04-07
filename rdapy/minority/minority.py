8058a52dac640a138703c1c88fda0fd6a7020491
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-04-07 06:05:11 -0700

    WIP

--- rdapy/minority/minority.py
+++ rdapy/minority/minority.py
@@ -67,33 +67,6 @@ def est_minority_opportunity(
     return oppty
 
 
-# TODO - DELETE
-# def est_minority_opportunity(mf: float, demo: Optional[str] = None) -> float:
-#     """Estimate the opportunity for a minority representation.
-
-#     NOTE - Shift minority proportions up, so 37% minority scores like 52% share,
-#       but use the uncompressed seat probability distribution. This makes a 37%
-#       district have a ~70% chance of winning, and a 50% district have a >99% chance.
-#       Below 37 % has no chance.
-#     NOTE - Sam Wang suggest 90% probability for a 37% district. That seems a little
-#       too abrupt and all or nothing, so I backed off to the ~70%.
-#     """
-
-#     assert mf >= 0.0
-
-#     range: list[float] = [0.37, 0.50]
-
-#     shift: float = 0.15  # For Black VAP % (and Minority)
-#     dilution: float = 0.50  # For other demos, dilute the Black shift by half
-#     if demo and (demo not in ["black", "minority"]):
-#         shift *= dilution
-
-#     wip_num: float = mf + shift
-#     oppty: float = 0.0 if (mf < range[0]) else min(est_seat_probability(wip_num), 1.0)
-
-#     return oppty
-
-
 def calc_minority_opportunity(
     statewide_demos: dict[str, float],
     demos_by_district: list[dict[str, float]],
