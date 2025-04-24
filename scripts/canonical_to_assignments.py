cad955cdddd67d47507156faf33e601df3c230d4
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-04-24 06:04:59 -0700

    WIP

--- scripts/canonical_to_assignments.py
+++ scripts/canonical_to_assignments.py
@@ -19,6 +19,8 @@ from typing import Any, Dict, List
 import argparse
 from argparse import ArgumentParser, Namespace
 
+from gerrychain import Graph
+
 from rdapy import (
     load_data,
     load_graph,
@@ -35,6 +37,7 @@ def main():
 
     args = parse_arguments()
 
+    recom_graph: Graph = Graph.from_json(args.graph)
     # adjacency_graph: Dict[str, List[str]] = load_graph(args.graph)
 
     with smart_read(args.input) as input_stream:
