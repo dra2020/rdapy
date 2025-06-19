20f606a16586c00c64fb6f35ab6713380a16c257
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-06-19 06:16:12 -0700

    WIP

--- scripts/graphs/compare_graphs.py
+++ scripts/graphs/compare_graphs.py
@@ -12,6 +12,7 @@ $ scripts/graphs/compare_graphs.py \
 from typing import List, Dict
 
 import os
+from itertools import combinations
 
 from rdapy import DISTRICTS_BY_STATE, load_graph
 
@@ -19,58 +20,82 @@ from rdapy import DISTRICTS_BY_STATE, load_graph
 def main() -> None:
     """Check whether two sets of adjacency graph are the same."""
 
-    # dir1: str = "~/local/adjacency-graphs"
-    dir1: str = "~/local/dra-to-publish"
-    # dir2: str = "~/local/dra-to-publish"
-    dir2: str = "~/local/dra-data/data"
+    graph_dirs: List[str] = ["vtd_data", "dra_data", "adjacency-graphs"]
+    combos: List[tuple] = list(combinations(graph_dirs, 2))
 
-    print(f"Comparing graphs in {dir1} and {dir2}.")
+    for dir1, dir2 in combos:
+        print(f"Comparing graphs in {dir1} and {dir2}.")
 
-    diff_geoids: List[str] = list()
-    diff_connections: List[str] = list()
-    bad_graphs: List[str] = list()
+        diff_geoids: List[str] = list()
+        diff_connections: List[str] = list()
+        bad_graphs: List[str] = list()
 
-    for xx, _ in DISTRICTS_BY_STATE.items():
-        try:
-            file1: str = f"{xx}_2020_graph.json"
-            # file2: str = f"{xx}_2020_graph.json"
-            file2: str = f"contiguity.json"
+        for xx, _ in DISTRICTS_BY_STATE.items():
+            try:
+                graph_path = path_to_graph(dir1, xx)
+                graph1: Dict[str, List[str]] = load_graph(graph_path)
 
-            graph_path = os.path.expanduser(dir1 + "/" + file1)
-            graph1: Dict[str, List[str]] = load_graph(graph_path)
+                graph_path = path_to_graph(dir2, xx)
+                graph2: Dict[str, List[str]] = load_graph(graph_path)
 
-            # graph_path = os.path.expanduser(dir2 + "/" + file2)
-            graph_path = os.path.expanduser(dir2 + f"/{xx}/2020_VD/" + file2)
-            graph2: Dict[str, List[str]] = load_graph(graph_path)
+            except Exception as e:
+                bad_graphs.append(xx)
+                continue
 
-        except Exception as e:
-            bad_graphs.append(xx)
-            continue
+            if set(graph1.keys()) != set(graph2.keys()):
+                diff_geoids.append(xx)
+                continue
 
-        if set(graph1.keys()) != set(graph2.keys()):
-            diff_geoids.append(xx)
-            continue
+            for geoid in graph1.keys():
+                if (
+                    set(graph1[geoid]) != set(graph2[geoid])
+                    and xx not in diff_connections
+                ):
+                    diff_connections.append((xx))
 
-        for geoid in graph1.keys():
-            if set(graph1[geoid]) != set(graph2[geoid]) and xx not in diff_connections:
-                diff_connections.append((xx))
+        if diff_geoids:
+            print(f"These graphs differ in some geoids: {diff_geoids}.")
+        else:
+            print("These graphs have the same geoids.")
 
-    if diff_geoids:
-        print(f"These graphs differ in some geoids: {diff_geoids}.")
-    else:
-        print("These graphs have the same geoids.")
+        if diff_connections:
+            print(f"These graphs differ in some connections: {diff_connections}.")
+        else:
+            print("These graphs have the same connections.")
 
-    if diff_connections:
-        print(f"These graphs differ in some connections: {diff_connections}.")
-    else:
-        print("These graphs have the same connections.")
-
-    if bad_graphs:
-        print(f"These graphs could not be loaded: {bad_graphs}.")
-    else:
-        print("All graphs loaded successfully.")
+        if bad_graphs:
+            print(f"These graphs could not be loaded: {bad_graphs}.")
+        else:
+            print("All graphs loaded successfully.")
+        print()
 
     pass
+
+
+def path_to_graph(dir: str, xx: str) -> str:
+    """Return the path to the graph directory."""
+
+    graph_path: str
+
+    if dir == "vtd_data":
+        graph_path = os.path.expanduser(
+            f"~/dev/vtd_data/2020_VTD/{xx}/{xx}_2020_graph.json"
+        )
+        return graph_path
+
+    if dir == "dra_data":
+        graph_path = os.path.expanduser(
+            f"~/local/dra-data/data/{xx}/2020_VD/contiguity_graph.json"
+        )
+        return graph_path
+
+    if dir == "adjacency-graphs":
+        graph_path = os.path.expanduser(
+            f"~/local/adjacency-graphs/{xx}_2020_graph.json"
+        )
+        return graph_path
+
+    assert False, f"Unknown directory: {dir}"
 
 
 if __name__ == "__main__":
