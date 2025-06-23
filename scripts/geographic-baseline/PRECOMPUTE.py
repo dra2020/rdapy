545874d7cdcb05a4533222f7d87b6b8a50e5b98f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-06-22 21:08:08 -0700

    WIP

---
+++ scripts/geographic-baseline/PRECOMPUTE.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+"""
+TODO - PRECOMPUTE BASELINES FOR ALL STATES
+
+"""
+
+import argparse
+from argparse import ArgumentParser, Namespace
+
+from typing import List
+
+from rdapy import DISTRICTS_BY_STATE
+
+states: List[str] = [
+    "AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+]
+states = ["NC"]
+
+
+def main():
+    """Precompute geographic baselines for all states & chambers."""
+
+    args = parse_arguments()
+
+    version: str = "v06"  # args.version
+    neighborhoods_dir: str = "~/local/neighborhoods"  # args.neighborhoods
+    baselines_dir: str = "~/local/precomputed"  # args.baselines
+
+    #
+
+    for xx, metadata in DISTRICTS_BY_STATE.items():
+
+        # TODO - Download the GeoJSON
+
+        # TODO - Unzip it
+
+        for chamber, ndistricts in metadata.items():
+            if chamber == "congress" and ndistricts == 1:
+                continue
+            if ndistricts is None:
+                continue
+
+            # TODO - Precompute the baselines for the state & chamber
+
+    pass
+
+
+def parse_arguments():
+    """Parse command line arguments."""
+
+    parser: ArgumentParser = argparse.ArgumentParser(
+        description="Parse command line arguments."
+    )
+
+    parser.add_argument(
+        "--version",
+        type=str,
+        help="Version of the GeoJSON data to use",
+    )
+    parser.add_argument(
+        "--neighborhoods",
+        type=str,
+        help="Directory where input neighborhood files are stored",
+    )
+    parser.add_argument(
+        "--baselines",
+        type=str,
+        help="Directory where output baseline files should be stored",
+    )
+
+    parser.add_argument("--debug", dest="debug", action="store_true", help="Debug mode")
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
+    )
+
+    args: Namespace = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
+
+### END ###
--- scripts/geographic-baseline/precompute_baselines.py
+++ scripts/geographic-baseline/precompute_baselines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-DO ONE-TIME CALCULATIONS
+PRECOMPUTE ONE-TIME CALCULATIONS
 
 For example:
 
