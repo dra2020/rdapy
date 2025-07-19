cc251cf579d266d3db9d745b37d189e30009fd9a
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-07-19 07:33:30 -0700

    WIP

--- scripts/data/make_census_json.py
+++ scripts/data/make_census_json.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 """
-EXTRACT THE TOTAL POPULATION FROM A CENSUS 
-from DRA's DEMOGRAPHIC DATA in their vtd_data REPO
-for USE IN CREATING RECOM GRAPHS
+MAKE A GEOID:POPULATION JSON FILE
+from the JSONL data extracted from a DRA GeoJSON file 
+
+This is a helper script so that other legacy scripts don't need to be modified.
 
 For example:
 
-$ scripts/extract_census.py \
---source ../../local/vtd_data/2020_VTD/NC/Demographic_Data_NC.v04.zip \
---output ../vtd_data/2020_VTD/NC/NC_2020_census.json \
---no-debug
+$ scripts/data/make_census_json.py \
+--input /path/to/input_data.jsonl \
+--output /path/to/output/census.json 
 
 For documentation, type:
 
-$ scripts/extract_demographic_data.py -h
+$ scripts/data/make_census_json.py -h
 
 """
 
@@ -25,80 +25,70 @@ from typing import Any, List, Dict
 
 import os
 
-from vtd_data import (
-    require_args,
-    write_json,
-    convert_filename,
-    zipped_csv_rows,
-)
+from rdapy import load_data, write_json
 
 
 def main() -> None:
-    """Extract census & demographic data from DRA's zipfile published in dra2020/vtd_data.."""
+    """Make a census.json file from the JSONL input data for a state."""
 
     args: Namespace = parse_args()
 
-    csv_file: str = convert_filename(os.path.basename(args.source))
-    dataset: str = "T_20_CENS"
-    field_label: str = "Total"
-    total_pop_field: str = f"{dataset}_{field_label}"
+    data_map: Dict[str, Any]
+    input_data: List[Dict[str, Any]]
+    data_map, input_data = load_data(args.input)
 
-    # Extract the census data
+    total_pop_field: str = f"{args.dataset}_{args.field}"
+    pop_by_geoid: Dict[str, int] = dict()
+    for row in input_data:
+        if "_tag_" not in row:
+            continue
+        if row["_tag_"] == "metadata":
+            continue
+        assert row["_tag_"] == "precinct", "Input data must be precincts."
 
-    census_data: Dict[str, int] = dict()
-    for row in zipped_csv_rows(args.source, csv_file):
-        census_data[row[args.geoid]] = int(row[total_pop_field])
+        geoid: str = row["data"]["geoid"]
+        pop: int = row["data"][total_pop_field]
+        pop_by_geoid[geoid] = pop
 
-    write_json(args.output, census_data)
+    write_json(args.output, pop_by_geoid)
 
 
 def parse_args() -> Namespace:
     parser: ArgumentParser = argparse.ArgumentParser(
-        description="Extract census data from a dra2020/vtd_data CSV file."
+        description="Parse command line arguments."
     )
 
     parser.add_argument(
-        "--source",
-        help="The input zip file containing the data in a CSV file",
+        "--input",
+        help="The JSONL input data file",
         type=str,
+        required=True,
     )
     parser.add_argument(
         "--output",
-        help="Where to write the output JSON file",
+        help="The output JSON file",
         type=str,
+        required=True,
     )
 
     parser.add_argument(
-        "--cycle",
-        default="2020",
-        help="The redistricting cycle (e.g., 2020)",
+        "--dataset",
         type=str,
+        default="T_20_CENS",
+        help="The input JSONL file",
     )
     parser.add_argument(
-        "--geoid",
-        default="GEOID20",
-        help="The geoid field",
+        "--field",
         type=str,
+        default="Total",
+        help="The total population census field",
     )
 
     parser.add_argument(
         "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
     )
 
-    # Enable debug/explicit mode
-    parser.add_argument("--debug", default=True, action="store_true", help="Debug mode")
-    parser.add_argument(
-        "--no-debug", dest="debug", action="store_false", help="Explicit mode"
-    )
-
     args: Namespace = parser.parse_args()
-
-    # Default values for args in debug mode
-    debug_defaults: Dict[str, Any] = {
-        "source": "../../local/vtd_data/2020_VTD/NC/Demographic_Data_NC.v04.zip",
-        "output": "../vtd_data/2020_VTD/NC/NC_2020_census.json",
-    }
-    args = require_args(args, args.debug, debug_defaults)
 
     return args
 
