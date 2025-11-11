15d6afea5b21ccf77d2f02b344d8c86fa49e817f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2025-11-11 12:18:38 -0800

    Added multi-member district scoring experiment

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ python setup.py register sdist upload
 
 setup(
     name="rdapy",
-    version="3.1.0",
+    version="3.2.0",
     description="Redistricting analytics in Python",
     url="https://github.com/dra2020/rdapy",
     author="alecramsay",
